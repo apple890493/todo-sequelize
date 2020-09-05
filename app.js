9d8f5c812bb954afa074aee0aef2e16ff1f20395
nonoLin <denkanono@gmail.com>
2020-09-05 17:57:35 +0800

    check file

--- app.js
+++ app.js
@@ -2,41 +2,21 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-Parser')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
+
+const userPassport = require('./config/passport')
+const routes = require('./routes')
 
 const app = express()
 const PORT = 3000
 
+userPassport(app)
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  res.send('hi')
-})
 
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-app.post('/users/login', (req, res) => {
-  res.render('login')
-})
-
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
-})
-
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`running on localhost:${PORT}`)
---
+++ routes/modules/todos.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+
+const db = require('../../models')
+const Todo = db.Todo
+
+
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+
+  console.log(UserId, name)
+
+  return res.render('new')
+})
+
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+module.exports = router
